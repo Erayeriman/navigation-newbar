0bdff966b10b7c725adcd259a159b74e6087fac9
Eray Eriman <eray.eriman@outlook.com.tr>
2023-04-12 22:08:04 +0300

    newbar-navigation

---
+++ javascript.js
@@ -0,0 +1,11 @@
+const bar=document.querySelector('.container')
+const items=document.querySelectorAll('.item')
+items.forEach(item=>{
+    item.addEventListener('click',()=>{
+        items.forEach(item=>{
+            item.classList.remove('active')
+        })
+        item.classList.add('active')
+
+    })
+})
